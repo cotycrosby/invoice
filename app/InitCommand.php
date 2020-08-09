0ca943925e11c0904ea77de012485949a203fb60
unknown <coty.crosby@gmail.com>
2020-08-09 04:47:11 -0400

    Created the command for Init functionality

---
+++ app/InitCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InitCommand extends Command 
+{
+	protected static $defaultName = 'init';
+
+	protected function configure() {
+		$this->setDescription('Creates the SQLITE file in the current directory');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		if(DatabaseFile::exists()){
+			$output->writeln('<fg=red;>The database file already exists. If you initialize it it will wipe the data.</>');
+			$output->writeln('');
+			$output->writeln('If you wish to proceed, run: <bg=red;fg=white;>rm .invoice.db</>');
+			return 1;
+		}
+
+		DatabaseFile::create();
+		DatabaseFile::bootEloquent();
+		DatabaseFile::initialize();
+		$output->writeln('<fg=green;>Database created successfully!</>');
+		
+		return 0;
+	}
+}
